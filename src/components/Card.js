c3327ae9ab08a09bdbfb5fbeddf5c04b8517db77
Namratha Dirsumilli <namrathadirsumilli@gmail.com>
2026-01-27 01:17:52 +0530

    Fix Home page cards layout

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,11 @@ function Card({ icon, title, desc, path }) {
 
   return (
     <div className="card" onClick={() => navigate(path)}>
-      <h3>{icon} {title}</h3>
+      <h3>
+        <span style={{ fontSize: "26px" }}>{icon}</span>
+        <br />
+        {title}
+      </h3>
       <p>{desc}</p>
     </div>
   );
